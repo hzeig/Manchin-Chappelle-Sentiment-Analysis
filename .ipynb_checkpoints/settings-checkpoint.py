841e32444d12f38804646e11b145c2c0186c97a2
hadar <hzeigerson@gmail.com>
2021-10-27 12:00:06 -0400

    initializng reddit API

---
+++ .ipynb_checkpoints/settings-checkpoint.py
@@ -0,0 +1,7 @@
+ra = {'CLIENT_ID' : 'Uvg4zZxq4RqYscFKq2Cz0Q',
+              'SECRET_KEY' : 'vwfbP4LHzkcunapJGpTicpmQSGIO9g'}
+
+login = {'grant_type': 'password',
+         'username': 'hadarlovestrees',
+         'password': 'redDIT!1'}
+pw = {'redDIT!1'}
