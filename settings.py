546fbf48c6cfab834927f626d881136266aacf0b
hadar <hzeigerson@gmail.com>
2021-10-27 23:39:28 -0400

    Collected data using RedditExtractoR

--- settings.py
+++
@@ -1,7 +0,0 @@
-ra = {'CLIENT_ID' : 'Uvg4zZxq4RqYscFKq2Cz0Q',
-              'SECRET_KEY' : 'vwfbP4LHzkcunapJGpTicpmQSGIO9g'}
-
-login = {'grant_type': 'password',
-         'username': 'hadarlovestrees',
-         'password': 'redDIT!1'}
-pw = {'redDIT!1'}
